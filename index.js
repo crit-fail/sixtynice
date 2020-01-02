4bc92509d05a5e0f5a5c11da1f5db4d1c8cae939
lboenish <lboenish@somalogic.com>
2020-01-02 13:50:28 -0700

    No longer breaks inline styles for the fools who use them

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const elements = document.getElementsByTagName('*');
     element.childNodes.forEach(node => {
         if (node.nodeType === 3 && node.nodeValue.includes("69")) {
             node.nodeValue = node.nodeValue.replace(
-                /69/g,
+                /\b69/g,
                 "69 ⇦ Now That's Nice 😎"
             );
         }
