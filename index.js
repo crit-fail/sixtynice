d4876d4b67aa95d6ae2190e825c3b0322b52fcb5
lboenish <lboenish@somalogic.com>
2020-01-02 10:37:33 -0700

    Nice 😎

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("fella we made it");
+let elements = document.getElementsByTagName('*');
+
+for (let i = 0; i < elements.length; i++) {
+    let element = elements[i]
+
+    for (let j = 0; j < element.childNodes.length; j++) {
+        let node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+            let text = node.nodeValue;
+            let replaced = text.replace('69', 'Nice 😎');
+
+            if (replaced !== text) {
+                node.nodeValue = replaced;
+            }
+        }
+    }
+}
