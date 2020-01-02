fc5d1154819ccaccd61e33ccb947f057ba541e4d
lboenish <lboenish@somalogic.com>
2020-01-02 14:47:24 -0700

    NOW doesn't break styles

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const elements = document.getElementsByTagName('*');
+const elements = document.querySelectorAll("*:not(style)");
 [...elements].forEach(element => {
-    element.childNodes.forEach(node => {
-        if (node.nodeType === 3 && node.nodeValue.includes("69")) {
-            node.nodeValue = node.nodeValue.replace(
-                /\b69/g,
-                "69 ⇦ Now That's Nice 😎"
-            );
-        }
-    })
+  element.childNodes.forEach(node => {
+    if (node.nodeType === 3 && node.nodeValue.includes("69")) {
+      node.nodeValue = node.nodeValue.replace(
+        /\b69/g,
+        "69 ⇦ Now That's Nice 😎"
+      );
+    }
+  });
 });
