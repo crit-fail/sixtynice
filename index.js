6866ce3c3ef98685f7d40485ed7605ca263a9168
lboenish <lboenish@somalogic.com>
2020-01-02 13:03:42 -0700

    updates

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const elements = document.getElementsByTagName('*');
-const elementsArray = Array.from(elements);
-elementsArray.forEach(element => {
+[...elements].forEach(element => {
     element.childNodes.forEach(node => {
         if (node.nodeType === 3 && node.nodeValue.includes('69')) {
-            element.innerHTML = node.nodeValue = node.nodeValue.replace('69', '69 <-- Now <strong>That\'s</strong> Nice 😎');
+            element.innerHTML = node.nodeValue = node.nodeValue.replace('69', '69 <= Now <strong>That\'s</strong> Nice 😎');
         }
     })
 });
